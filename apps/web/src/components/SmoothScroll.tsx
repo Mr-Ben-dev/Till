580feeb89c155a75df5de41b589831c004bdd00d
Mr-Ben-dev <momowael122.asd@gmail.com>
2026-08-15 16:57:26 +0300

    error copy that a human can read

--- apps/web/src/components/SmoothScroll.tsx
+++ apps/web/src/components/SmoothScroll.tsx
@@ -1,3 +1,31 @@
-export function Sketch() {
-  return null
+import { useLayoutEffect, type ReactNode } from 'react'
+import Lenis from 'lenis'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import 'lenis/dist/lenis.css'
+
+gsap.registerPlugin(ScrollTrigger)
+
+export function SmoothScroll({ children }: { children: ReactNode }) {
+  useLayoutEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.15,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+    })
+    const onScroll = () => ScrollTrigger.update()
+    lenis.on('scroll', onScroll)
+    const tick = (time: number) => {
+      lenis.raf(time * 1000)
+    }
+    gsap.ticker.add(tick)
+    gsap.ticker.lagSmoothing(0)
+    requestAnimationFrame(() => ScrollTrigger.refresh())
+    return () => {
+      gsap.ticker.remove(tick)
+      gsap.ticker.lagSmoothing(500)
+      lenis.destroy()
+    }
+  }, [])
+  return <>{children}</>
 }
